c8623e95011fc1ed6540decbda76c31bbc303f28
lozano <lozano@lozano-VirtualBox>
2016-10-14 17:31:03 +0200

    First commit with program sctructure

---
+++ src/datavalidator/DataValidator.java
@@ -0,0 +1,13 @@
+/*
+ * This class will be used to create the functions that validates the data @params
+ */
+package datavalidator;
+
+/** 
+ * @author Joan Fernández & Jonathan Lozano
+ * @version 1.0
+ * @param 
+ */
+public class DataValidator {
+    
+}
---
+++ src/datavalidator/IndexValidator.java
@@ -0,0 +1,29 @@
+/*
+ * This class will be used to inicialize the data and call the functions that will validate them.
+ */
+package datavalidator;
+
+import java.util.Scanner;
+
+/**
+ * @author Joan Fernández & Jonathan Lozano
+ * @version 1.0
+ */
+public class IndexValidator {
+    Scanner scr = new Scanner(System.in);
+    
+    public static void main (String [] args)
+    {
+        IndexValidator ex = new IndexValidator();
+        ex.run();
+        
+    }
+    /** Works like the main and prevents static problems
+     * @author Joan Fernández & Jonathan Lozano
+     * @version 1.0
+     */
+    private void run()
+    {
+        
+    }
+}
